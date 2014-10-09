ea6ae0e37874a11aa0eed81f9fee1f3e8d9e6319
Viktor Farcic <viktor.farcic@everis.com>
2014-10-09 14:22:12 +0200

    Added Heroku Procfile

--- web.js
+++ web.js
@@ -2,7 +2,8 @@ var port = Number(process.env.PORT || 8000);
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname));
 var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
+    console.log('DIR ' + __dirname);
 });
